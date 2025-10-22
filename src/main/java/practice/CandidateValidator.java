4d09694aa0d6ecd513ed4fb8ec0490b695410fa2
Krzysztof Piatkowski <krzysztofpiatkowski1987@gmail.com>
2025-10-22 15:26:30 +0200

    Add fixes

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -5,13 +5,17 @@ import model.Candidate;
 import java.util.function.Predicate;
 
 public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_AGE = 35;
+    private static final int YEARS_IN_UKRAINE = 10;
+    private static final String REQUIRED_NATINALITY = "Ukrainian";
+
     public boolean test(Candidate candidate) {
         String[] years = candidate.getPeriodsInUkr().split("-");
         int start = Integer.parseInt(years[0]);
         int end = Integer.parseInt(years[1]);
 
-        return candidate.getAge() > 35 && candidate.isAllowedToVote()
-            && candidate.getNationality().equals("Ukrainian")
-            && (end - start) >= 10;
+        return candidate.getAge() >= MIN_AGE && candidate.isAllowedToVote()
+            && candidate.getNationality().equals(REQUIRED_NATINALITY)
+            && (end - start) >= YEARS_IN_UKRAINE;
     }
 }
