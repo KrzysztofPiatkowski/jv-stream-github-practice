3ecaea30d6c44bba750fd52f30c7613dc8736003
Krzysztof Piatkowski <krzysztofpiatkowski1987@gmail.com>
2025-10-22 14:57:28 +0200

    Task solution

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,17 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import model.Candidate;
+
+import java.util.function.Predicate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    public boolean test(Candidate candidate) {
+        String[] years = candidate.getPeriodsInUkr().split("-");
+        int start = Integer.parseInt(years[0]);
+        int end = Integer.parseInt(years[1]);
+
+        return candidate.getAge() > 35 && candidate.isAllowedToVote()
+            && candidate.getNationality().equals("Ukrainian")
+            && (end - start) >= 10;
+    }
 }
